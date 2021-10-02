7c3cae5a844743524683454d241e2d0a06d3ec3f
xrsec <troy@zygd.site>
2021-10-03 03:12:06 +0800

    2021/10/03/03 By xr

--- .github/code-server_version/version_checking.py
+++ .github/code-server_version/version_checking.py
@@ -4,6 +4,6 @@ import os, re, requests, sys
 code_server_version =  requests.get("https://api.github.com/repos/cdr/code-server/releases/latest").json()["tag_name"]
 if code_server_version == open(".github/code-server_version/code-server_version").read():
     print("It's the latest edition! version: " + code_server_version)
-    os._exit("It's the latest edition! version: " + code_server_version)
+    os._exit("It's the latest edition! version: ", code_server_version)
 else:
     open(".github/code-server_version/code-server_version", "w").write(code_server_version)
