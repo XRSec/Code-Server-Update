320219177c591a502856632e63742878e91fa7db
xrsec <troy@zygd.site>
2021-10-03 00:47:45 +0800

    2021/10/03/00 By xr

--- .github/code-server_version/version_checking.py
+++ .github/code-server_version/version_checking.py
@@ -2,8 +2,12 @@
 # _*_ coding: utf-8 _*_
 import os, re, requests, sys
 code_server_version =  requests.get("https://api.github.com/repos/cdr/code-server/releases/latest").json()["tag_name"]
-if code_server_version == open("code-server_version").read():
-    print("sure")
-else:
-    print(open("code-server_version").read())
+# if code_server_version == open("code-server_version").read():
+#     print("sure")
+# else:
+#     print(open("code-server_version").read())
+
+for path,dir_list,file_list in os.getcwd():
+    for file_name in file_list:
+        print(os.path.join(path, file_name) )
 # https://raw.githubusercontent.com/XRSec/Code-Server-Update/main/.github/code-server_version/code-server_version
