f34d938c21b947cf5cf7aeb90ac1d6c63cd7562e
星冉 <troy@zygd.site>
2021-12-27 16:18:43 +0800

    fix more

--- code-server.sh
+++ code-server.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/env sh
 clear
 echo -e "\033[1;35m #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#* \n\033[0m"
 # /usr/sbin/sshd -D &
@@ -11,6 +11,6 @@ echo -e "\033[1;35m /_/\_\ |_| \_\ |___/  \___|  \___| \n\033[0m"
 echo -e "\033[1;32m start code-server succers \033[0m"
 echo -e "\033[1;31m [help] \n\033[0m"
 echo -e "\033[1;36m Your" "`/usr/bin/grep password /root/.config/code-server/config.yaml | grep -v "auth: password"` \033[0m"
+echo -e "\033[1;36m Your" "`/usr/bin/grep bind-addr /root/.config/code-server/config.yaml` \033[0m"
 echo -e "\033[1;35m php56-fpm & nginx  \n\n\n\033[0m"
-/usr/bin/code-server >> /dev/null &
-/bin/bash
+/usr/bin/code-server
